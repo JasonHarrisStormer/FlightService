b7a2209ee7deb580321996902292385c931b9df7
Jason Harris <jharris@skillstorm.com>
2022-06-16 16:52:39 -0400

    setup react main page

---
+++ front/src/App.jsx
@@ -0,0 +1,21 @@
+import { Form } from "./Form";
+import './App.css';
+
+
+const App = () =>{
+
+
+    return(
+        <>
+            <div className="App">
+            <header header className="App-header">
+                <img src="https://media.istockphoto.com/vectors/plane-icon-vector-id694972110?k=20&m=694972110&s=612x612&w=0&h=B9s-39VnZb--aEaD4XJjU0AovxVIWzpGdAMj6ynY98U=" 
+                        className="App-logo" alt="logo" height="300"/>
+                        <h1><div>Flight Services</div></h1></header>       
+            <Form></Form>
+            </div>      
+        </>
+    );
+}
+
+export default App;
